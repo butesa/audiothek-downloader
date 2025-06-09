3f5b96e1a2035754d0154c3849c6eaae0e2d5645
Lukas Schulte <github@das-it-gesicht.de>
2025-06-09 15:26:05 +0200

    feat(main): add support for rename templates

--- audiothek.py
+++ audiothek.py
@@ -6,10 +6,23 @@ import sys
 from pathlib import Path
 
 import requests
+import yaml
 from pathvalidate import sanitize_filename
 
 API_URL = 'https://api.ardaudiothek.de/graphql'
 GRAPHQL_DIR = os.path.join(Path(__file__).parent, 'graphql')
+RENAME_TEMPLATES = os.path.join(Path(__file__).parent, 'rename_templates')
+
+
+def rename(title: str, program_id: str, kind: str) -> str:
+    template_path = os.path.join(RENAME_TEMPLATES, f'{program_id}.yaml')
+    if os.path.isfile(template_path):
+        with open(template_path, 'r') as f:
+            template = yaml.safe_load(f)
+        if template.get(kind):
+            title = re.sub(template.get(kind).get('regex'), template.get(kind).get('substitution'), title)
+    title = sanitize_filename(title)
+    return title.strip()
 
 
 def download_episodes(show_id: int, directory: str):
@@ -31,44 +44,41 @@ def download_episodes(show_id: int, directory: str):
     nodes: list = response_json.get('data').get('result').get('items').get('nodes')
 
     for i, node in enumerate(nodes):
-        title: str = node.get('title')
-        filename: str = sanitize_filename(title.replace('/', '-'))
+        show_id: str = node.get('programSet').get('id')
+        item_title: str = node.get('title')
+
+        filename: str = rename(item_title, show_id, 'fileTemplate')
+        show_title: str = rename(node.get('programSet').get('title'), show_id, 'showTemplate')
+
+        mp3_url: str = node.get('audios')[0].get('downloadUrl') or node.get('audios')[0].get('url')
+
+        show_path: str = os.path.join(directory, rename(show_title, show_id, 'showTemplate'))
 
         if args.square_images:
             image_attr = 'url1X1'
         else:
             image_attr = 'url'
         image_url: str = node.get('image').get(image_attr).replace('{width}', '500')
-        mp3_url: str = node.get('audios')[0].get('downloadUrl') or node.get('audios')[0].get('url')
 
-        program_set_id: str = node.get('programSet').get('id')
-        program_set_title: str = node.get('programSet').get('title')
-        program_path: str = os.path.join(directory, f'{program_set_title} ({program_set_id})')
+        if args.group_episodes:
+            episode_name = rename(item_title, show_id, 'episodeDirectoryTemplate')
+            show_path: str = os.path.join(show_path, episode_name)
 
-        episode_title_regex = re.match(r'^(.+?)\s?\(\d+/\d+\)$', title)
-        if episode_title_regex and args.group_episodes:
-            episode_title: str = episode_title_regex.group(1)
-            episode_title_safe: str = sanitize_filename(episode_title.replace('/', '-')).strip()
-
-            program_path: str = os.path.join(program_path, episode_title_safe)
-
-        # get information of program
-        if program_set_id:
-            if not os.path.exists(program_path):
+        if show_id:
+            if not os.path.exists(show_path):
                 try:
-                    os.makedirs(program_path)
+                    os.makedirs(show_path)
                 except Exception as e:
                     print(f'[Error] Couldn\'t create output directory: {e}', file=sys.stderr)
                     continue
 
-            image_file_path = os.path.join(program_path, f'{filename}.jpg')
+            image_file_path: str = os.path.join(show_path, f'{filename}.jpg')
+            mp3_file_path: str = os.path.join(show_path, f'{filename}.mp3')
 
             if not os.path.exists(image_file_path):
                 response_image = requests.get(image_url)
                 with open(image_file_path, 'wb') as f:
                     f.write(response_image.content)
-
-            mp3_file_path: str = os.path.join(program_path, f'{filename}.mp3')
 
             print(f'Download: {i + 1} of {len(nodes)} -> {mp3_file_path}')
 
@@ -80,7 +90,7 @@ def download_episodes(show_id: int, directory: str):
                 with open(mp3_file_path, 'wb') as f:
                     f.write(response_mp3.content)
         else:
-            print('No program_set_id found!', file=sys.stderr)
+            print('No show_id found!', file=sys.stderr)
 
 
 if __name__ == '__main__':
